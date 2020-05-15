97ff4d181d68863b3cad2b11334f7b6e96fba4d2
leovdn <leo_vdn@hotmail.com>
2020-05-15 19:34:48 -0300

    window scroll animation

--- script.js
+++ script.js
@@ -59,3 +59,24 @@ function startSmoothScroll() {
     })
 }
 startSmoothScroll();
+
+function startAnimateScroll() {
+    const sections = document.querySelectorAll('.js-scroll');
+
+    if (sections.length) {
+        const windowHalf = window.innerHeight * 0.7;
+
+        function animemateScroll() {
+            sections.forEach((section) => {
+                const sectionTop = section.getBoundingClientRect().top;
+                const isSectionVisible = (sectionTop - windowHalf) < 0;
+                if (isSectionVisible) {
+                    section.classList.add('active');
+                }
+            })
+        }
+        animemateScroll();
+        window.addEventListener('scroll', animemateScroll);
+    }
+}
+startAnimateScroll();
