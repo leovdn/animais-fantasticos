45c4339db81a2b0e099f412210552a0db65cd0d7
leovdn <leo_vdn@hotmail.com>
2020-05-15 19:08:35 -0300

    Smooth Scroll through sections

--- script.js
+++ script.js
@@ -38,5 +38,24 @@ function startAccordion() {
         })
     }
 }
-
 startAccordion();
+
+function startSmoothScroll() {
+    const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+    function scrollSection(event) {
+        event.preventDefault();
+        const href = event.currentTarget.getAttribute('href');
+        const section = document.querySelector(href);
+
+        section.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+        })
+    }
+
+    linksInternos.forEach((link) => {
+        link.addEventListener('click', scrollSection);
+    })
+}
+startSmoothScroll();
