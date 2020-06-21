74528b1dd8d867641ca14e7304b1e93f5c941af4
leovdn <leo_vdn@hotmail.com>
2020-06-21 05:52:55 -0300

    created a Login modal

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@ import animateMenu from './modules/menu-animation.js';
 import accordion from './modules/accordion.js';
 import animateScroll from './modules/scroll-animation.js';
 import smooth from './modules/smooth-animation.js';
+import initModal from './modules/modal.js';
 
 animateMenu();
 accordion();
 animateScroll();
 smooth();
+initModal();
